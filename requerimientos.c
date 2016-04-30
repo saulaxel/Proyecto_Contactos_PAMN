e1e3f340f4ecb05f43831cfbfdad0669a0751510
Martínez Ortiz Saúl Axel <saulaxel.50@gmail.com>
2016-04-29 22:59:37 -0500

    nuevo programa
    Programa que cumple con los requerimientos.

---
+++ requerimientos.c
@@ -0,0 +1,29 @@
+/*
+ * Primer Proyecto Programación Avanzada y metodos numéricos
+ * "Sistema de seguridad para contactos"
+ * Parte 0: Pedir contactos al usuario y guardarlos 
+    * Este programa es parecido al llamado "Añadir_y_Codificar.c" pero con bastantes
+    * menos caracteristicas, debido a que el programa antes mencionado fue el desarrollo
+    * original de nuestro equipo pensado antes de disponer de la documentación con los 
+    * requerimientos contcretos del proyecto. El programa original es bastante completo 
+    * y cuenta con diversas caracteristicas extras con respecto a los requerimientos 
+    * originales del proyecto.
+    * Sin embargo el programa original no cumple con uno de los requerimientos originales: 
+    * memoria dinámica al crear el arreglo decontactos.
+    * A pesar de tratarse de un detalle simple, el añadirlo a estas alturas al
+    * programa original resultaría problematico dado que este trabaja con un menú
+    * que permite añadir contactos cuantas veces se quiera, detalle que de trabajarse 
+    * junto con memoria dinámica, requeriría el uso de una estructura de datos para 
+    * guardar los contactos que se van creando dinamicamente uno a uno (ya que no es 
+    * posible garantizar la adyacencia en memoria de dichos contactos a menos que se 
+    * crearan en una sola excibición) con lo que se tendrían que restructurar muchas 
+    * cosas en el programa.
+    * Por lo tanto, este programa es un añadido a los originalmente creados con el cual
+    * se logran cumplir fielmente los requerimientos originales y nada más que dichos
+    * requerimientos con el fin de no quedar faltos en la entrega.
+ * Integrantes del equipo:
+ * SIERRA GONZÁLEZ HÉCTOR ALEJANDRO
+ * CABRERA LÓPEZ OSCAR EMILIO
+ * MENDOZA GARCÍA ULISES
+ * MARTÍNEZ ORTIZ SAÚL AXEL
+ */
